2723005c5a20821a72e1e2f7c48ffb73b43f1444
JsonChao <chao.qu521@gmail.com>
2025-05-20 18:53:13 +0800

    修复: 移除Footer组件中未使用的导入以解决ESLint错误

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,3 @@
-import TwitterSVG from "../../public/twitter.svg";
-import GithubSVG from "../../public/Github.svg";
-import Image from "next/image";
-
 export default function Footer() {
   return (
     <footer className="mt-auto px-4 py-3 text-center text-xs text-gray-400">
